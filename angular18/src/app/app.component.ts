4e5a6016bfc76481ba8b06f1d696fe2fe630edc3
softprotechcoader <softprotechcoader@outlook.com>
2024-10-24 07:07:37 +0000

    Api Get Method.

--- angular18/src/app/app.component.ts
+++ angular18/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DatabindingComponent } from "./databinding/databinding.component";
+import { PipeComponent } from "./pipe/pipe.component";
+import { GetApiComponent } from "./get-api/get-api.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DatabindingComponent],
+  imports: [RouterOutlet, DatabindingComponent, PipeComponent, GetApiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ angular18/src/app/get-api/get-api.component.ts
@@ -0,0 +1,40 @@
+// import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject } from '@angular/core';
+
+@Component({
+  selector: 'app-get-api',
+  standalone: true,
+  imports: [],
+  templateUrl: './get-api.component.html',
+  styleUrl: './get-api.component.css'
+})
+export class GetApiComponent {
+
+  userList:any [] =[];
+  customerList:any[]=[];
+
+  // Two ways by which we can inject either Inject Method or Dependency Injection
+// http = Inject(HttpClient);    // From Angular 16 onwords.
+constructor(private http:HttpClient){}
+
+// https://jsonplaceholder.typicode.com/users    --> Fake Api from jsonplaceholder
+
+// https://projectapi.gerasim.in/api/RealEstate/GetAllCustomers   -->API from https://projectapi.gerasim.in/index.html.
+
+getAllUser(){
+  debugger;
+  this.http.get("https://jsonplaceholder.typicode.com/users").subscribe((res:any)=>{
+    debugger;
+    this.userList = res;
+  });
+}
+
+getAllCustomer(){
+  this.http.get("https://projectapi.gerasim.in/api/RealEstate/GetAllCustomers").subscribe((res:any)=>{
+    this.customerList = res.data;
+  })
+}
+
+
+}
